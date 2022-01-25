ee9bedae00939cac84c6221ac4733eda3c833479
Aria Nur Jamal <jamal.psw@gmail.com>
2022-01-25 21:37:08 +0700

    git configuration

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
+import {Fragment} from 'react';
 import Header from './components/Layout/Header';
 
 function App() {
   return (
-    <div>
+    <Fragment>
       <Header />
-    </div>
+    </Fragment>
   );
 }
 
