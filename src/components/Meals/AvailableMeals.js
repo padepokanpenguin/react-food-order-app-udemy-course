50c0d108d7f19ddde31908628679f138725e3742
Aria Nur Jamal <jamal.psw@gmail.com>
2022-03-05 08:06:48 +0700

    handling error

--- src/components/Meals/AvailableMeals.js
+++ src/components/Meals/AvailableMeals.js
@@ -5,10 +5,17 @@ import classes from './AvailableMeals.module.css';
 
 const AvailableMeals = () => {
 	const [meals, setMeals] = useState([]);
+	const [isLoading, setIsLoading] = useState(true);
+	const [error, setError] = useState(undefined);
 
 	useEffect(() => {
 		const fetchMeals = async () => {
 			const response = await fetch('https://react-food-app-f788b-default-rtdb.asia-southeast1.firebasedatabase.app/meals.json');
+			
+			if (!response.ok) {
+				throw new Error('something went wrong!');
+			}
+
 			const responseData = await response.json();
 
 			const loadedMeals = [];
@@ -22,10 +29,27 @@ const AvailableMeals = () => {
 				});
 			}
 			setMeals(loadedMeals);
+			setIsLoading(false)
+
 		};
 
-		fetchMeals();
+			fetchMeals().catch(error => {
+				setIsLoading(false)
+				setError(error.message)
+			});		
 	}, [])
+
+	if (isLoading){
+		return<section className={classes.MealsLoading}>
+			<p>Loading.....</p>
+		</section>
+	};
+
+	if (error) {
+		<section className={classes.httpError}>
+			<p>{error}</p>
+		</section>
+	}
 
 	const mealsList = meals.map((meal) => <MealItem key={meal.id} id={meal.id} name={meal.name} description={meal.description} price={meal.price} />);
 		
